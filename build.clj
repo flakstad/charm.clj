8c298ea78b864584732cb09034e4ff973ee163b2
Timo Kramer <fw-git@timokramer.de>
2026-02-15 11:47:59 +0100

    ci: fix config user

--- build.clj
+++ build.clj
@@ -52,8 +52,8 @@
 
 (defn tag-release [_]
   (let [tag (tag-str)
-        git-name (or (System/getenv "GIT_COMMITTER_NAME") "github-actions[bot]")
-        git-email (or (System/getenv "GIT_COMMITTER_EMAIL") "41898282+github-actions[bot]@users.noreply.github.com")]
+        git-name (or (System/getenv "GIT_COMMITTER_NAME") "Timo Kramer")
+        git-email (System/getenv "GIT_COMMITTER_EMAIL")]
     (b/git-process {:git-args ["-c" (str "user.name=" git-name)
                                "-c" (str "user.email=" git-email)
                                "tag" "-a" tag "-m" (str "Release " tag)]})
